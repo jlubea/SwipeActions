0c6fbca350702f363ae0bb002e0d60cf4b62cd95
Alexander Kraev <aleksandr.kraev@lenta.com>
2024-09-28 12:36:18 +0300

    no message

--- Sources/SwipeActions/Environment keys/SensitivityKey.swift
+++ Sources/SwipeActions/Environment keys/SensitivityKey.swift
@@ -17,7 +17,7 @@ public extension View {
     ///
     /// - Parameters:
     ///   - sensitivity: A sensitivity that will be used in  ``SwipeAction`` drag gesture.
-    ///    Pass `nil` to ignore environment sensitivity and to allow the system to provide its own size.
+    ///    Pass `nil` to ignore environment sensitivity and to allow the system to provide its own sensitivity.
     func swipeSensitive(_ sensitivity: Sensitivity?) -> some View {
         environment(\.sensitivity, sensitivity?.value)
     }
